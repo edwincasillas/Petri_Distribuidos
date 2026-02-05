408579f7383f07b41b7f6cebfd74b4438fa37340
Juan Pascual <juanma.pas27@gmail.com>
2026-02-04 21:22:05 -0600

    Added Prefixes and Suffixes on afn

--- generate_sets.py
+++ generate_sets.py
@@ -2,24 +2,26 @@ def generate_bef(lambda_, start_symbol, end_symbol):
     """
     Generar la relación de precedencia BEF(λ) ⊆ (Σ x Σ) x {oi}
     """
-    bef = set()
+    bef = []
     
     for trace_name, events in lambda_:
         # Primer evento
         if events:
-            bef.add(((start_symbol, events[0]), trace_name))
+            bef.append(((start_symbol, events[0]), trace_name))
         # Eventos consecutivos
         for i in range(len(events) - 1):
-            bef.add(((events[i], events[i + 1]), trace_name))
+            event = ((events[i], events[i + 1]), trace_name)
+            if event not in bef:
+                bef.append(event)
         # Ultimo evento
         if events:
-            bef.add(((events[-1], end_symbol), trace_name))
+            bef.append(((events[-1], end_symbol), trace_name))
     
     return bef
 
 def generate_po(bef, start_symbol, end_symbol):
     """
-    Generar la relación de orden parcial PO(λ) = BEF(λ) \ Conc
+    Generar la relación de orden parcial PO(λ) = BEF(λ) / Conc
         donde:
             Conc = {((a, b), oi), ((b, a), oj) ∈ BEF(λ) | oi ≠ oj }
     """
@@ -43,18 +45,37 @@ def generate_po(bef, start_symbol, end_symbol):
                 conc.add((b, a))
     
     # Hacer PO(λ) = BEF(λ) \ Conc
-    po = set()
+    po = []
     for events in bef:
         (a, b), trace_name = events
         if a != start_symbol and b != end_symbol and a != end_symbol and b != start_symbol:
             key = (a, b)
             if key not in conc:
-                po.add(events)
+                po.append(events)
         else:
             # Agregar primer y ultimo evento de las trazas
-            po.add(events)
+            po.append(events)
     
-    return po, conc
+    # "Desdoblar" ciclos
+    po_wo_loops = []
+    ori_paths = {}
+    first_ori_paths = {}
+
+    for (a, b), trace_name in po:
+        if trace_name not in ori_paths:
+            ori_paths[trace_name] = set()
+            first_ori_paths[trace_name] = {}
+        
+        new_b = b
+        if b in ori_paths[trace_name]:
+            new_b = f"{b}'"
+        po_wo_loops.append(((a, new_b), trace_name))
+        ori_paths[trace_name].add(a)
+
+        if b not in first_ori_paths[trace_name]:
+            first_ori_paths[trace_name][b] = a
+    
+    return po_wo_loops, conc
 
 def generate_afn(bef, start_symbol, end_symbol):
     q = set()
@@ -71,17 +92,16 @@ def generate_afn(bef, start_symbol, end_symbol):
     delta[end_symbol] = {}
 
     # Eliminar transicion epsilon inutil
-    new_bef = set()
+    new_bef = []
     finals = []
-    for ((a, b), c) in bef:
+    for (a, b), c in bef:
         if b == end_symbol:
             finals.append(f"({a}, {c})")
         else:
-            new_bef.add(((a, b), c))
-    bef = new_bef
+            new_bef.append(((a, b), c))
     
     # Agregar transicion con consumo de simbolo
-    for (a, b), trace in bef:
+    for (a, b), trace in new_bef:
         symbol = b
         sigma.add(symbol)
         
@@ -112,48 +132,128 @@ def generate_afn(bef, start_symbol, end_symbol):
             delta[origin][symbol] = set()
         delta[origin][symbol].add(dest)
     
+    return q, sigma, delta, q0, f, new_bef, finals
+
+def zip_afn(bef, finals, start_symbol, end_symbol):
+    trace_name = []
+    events = []
+    trace_event = []
+    for (a, b), c in bef:
+        if c not in trace_name:
+            if trace_name:
+                events.append(trace_event)
+            trace_event = []
+            trace_name.append(c)
+        trace_event.append(((a, b), c))
+    events.append(trace_event)
+
+    max_ = max([len(event) for event in events])
+
+    # Encontrar prefijos y sufijos comunes
+    prefixes = find_pre_su_fixes(max_, trace_name, events)
+    events = [list(reversed(sublist)) for sublist in events]
+    sufixes = find_pre_su_fixes(max_, trace_name, events)
+
+    # Crear AFN compacto
+    q = set()
+    sigma = set()
+    delta = {}
+    q0 = start_symbol
+    f = set()
+
+    q.add(q0)
+    delta[q0] = {}
+    
+    f.add(end_symbol)
+    q.add(end_symbol)
+    delta[end_symbol] = {}
+
+    # Mapeo de nuevos estado
+    mapping = {}
+    cont = 1
+    # Recorrer cada diccionario
+    for dic in prefixes:
+        for clave, valores in dic.items():
+            # Extraer el destino (segunda letra)
+            origin = clave.split(', ')[1].rstrip(')')
+            # Para cada TR en la lista, asignar el mismo Xn
+            for tr in valores:
+                clave_resultado = f'({origin}, {tr})'
+                mapping[clave_resultado] = f'({origin}, X{cont})'
+            # Incrementar Xn para el siguiente nivel
+            cont += 1
+    cont = 1
+    for dic in sufixes:
+        for clave, valores in dic.items():
+            # Extraer el origen (primera letra)
+            origin = clave.split(', ')[0].lstrip('(')
+            # Para cada traza en la lista, asignar el mismo Yn
+            for tr in valores:
+                clave_resultado = f'({origin}, {tr})'
+                mapping[clave_resultado] = f'({origin}, Y{cont})'
+            # Incrementar Yn para el siguiente nivel
+            cont += 1
+
+    # Agregar transicion con consumo de simbolo
+    for (a, b), trace in bef:
+        symbol = b
+        sigma.add(symbol)
+        
+        # Determinar estado inicial
+        if a == start_symbol:
+            origin = a
+        else:
+            origin = f"({a}, {trace})"
+        
+        # Determinar estado destino
+        dest = f"({b}, {trace})"
+        if dest in finals:
+            dest = end_symbol
+        
+        # Agregar estados a Q
+        if origin in mapping:
+            origin = mapping[origin]
+        if dest in mapping:
+            dest = mapping[dest]
+        q.add(origin)
+        q.add(dest)
+        
+        # Inicializar transiciones para origen
+        if origin not in delta:
+            delta[origin] = {}
+        # Inicializar transiciones para destino
+        if dest not in delta:
+            delta[dest] = {}
+        
+        # Agregar transición no determinista
+        if symbol not in delta[origin]:
+            delta[origin][symbol] = set()
+        delta[origin][symbol].add(dest)
+    
     return q, sigma, delta, q0, f
 
-def zip_afn(q, sigma, delta, q0, f):
-    # Encontrar prefijos comunes
-    prefixes = ['START']
-    new_states = {}
-    while prefixes:
-        for p in prefixes:
-            for symbol, states in delta[p].items():
-                if len(states) > 1:
-                    new = {}
-                    for state in states:
-                        n = delta.pop(state)
-                        for clave, valor in n.items():
-                            if clave in new:
-                                new[clave].update(valor)
-                            else:
-                                new[clave] = valor
-                    new_states[symbol] = states
-                    prefixes.append(symbol)
-                    delta[symbol] = new
-                    delta[p][symbol] = set(symbol)
-            prefixes.remove(p)
-    # Encontrar sufijos comunes
-    """ print(delta)
-    sufixes = []
-    for key, transitions in delta.items():
-        for value in transitions.values():
-            if isinstance(value, (dict, set, list)):
-                if 'END' in str(value):
-                    sufixes.append(key)
-            elif value == 'END':
-                sufixes.append(key)
-    print(sufixes)
-    for p in sufixes:
-        if len(delta[p]) == 1:
-            new_state = p.strip('()').split(',')[0].strip()
-            n = delta.pop(p)
-            if new_state in new:
-                new[new_state].update(n)
-            else:
-                new[new_state] = n
-        sufixes.remove(p) """
-
-    return set(delta.keys()), delta
+def find_pre_su_fixes(max_, trace_name, events):
+    # Encontrar prefijos/sufijos comunes
+    fixes = []
+    actual_fixes = {"START": trace_name}
+    for i in range(max_):
+        aux = {}
+        for a, b in actual_fixes.items():
+            indexes = []
+            for x in b:
+                indexes.append(trace_name.index(x))
+            # Nuevos simbolos posibles del prefix/sufix
+            for x in indexes:
+                (a2, b2), c2 = events[x][i]
+                aux[f"({a2}, {b2})"] = []
+            # Encontrar nuevas continuaciones de prefijos/sufijos
+            for x in indexes:
+                (a2, b2), c2 = events[x][i]
+                if f"({a2}, {b2})" in aux.keys():
+                    aux[f"({a2}, {b2})"].append(c2)
+            aux = {k: v for k, v in aux.items() if len(v) > 1}
+        if not aux:
+            break
+        actual_fixes = aux
+        fixes.append(actual_fixes)
+    return fixes
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     # Punto 1
     bef = generate_sets.generate_bef(lambda_, START_SYMBOL, END_SYMBOL)
     print("BEF")
-    print(bef)
+    print("BEF", bef)
 
     # Punto 2
     make_graphs.dibujar_grafo_precedencia(bef, START_SYMBOL, END_SYMBOL)
@@ -27,16 +27,17 @@ def main():
     make_graphs.dibujar_grafo_precedencia(po, START_SYMBOL, END_SYMBOL, "grafo de PO")
 
     # Punto 4
-    q, sigma, delta, q0, f = generate_sets.generate_afn(bef, START_SYMBOL, END_SYMBOL)
+    q, sigma, delta, q0, f, new_bef, finals = generate_sets.generate_afn(bef, START_SYMBOL, END_SYMBOL)
     print("AFN")
     print("q: ", q)
     print("sigma: ", sigma)
     print("delta: ", delta)
     print("q0: ", q0)
     print("f: ", f)
+    print("new_bef: ", new_bef)
     make_graphs.dibujar_afn(q, sigma, delta, q0, f)
 
-    q, delta = generate_sets.zip_afn(q, sigma, delta, q0, f)
+    q, sigma, delta, q0, f = generate_sets.zip_afn(new_bef, finals, START_SYMBOL, END_SYMBOL)
     print("AFN compacto")
     print("q: ", q)
     print("sigma: ", sigma)
